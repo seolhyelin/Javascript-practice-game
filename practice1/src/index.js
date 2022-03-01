a55ebb1176f9c717c3bc2ae52aee8816c2f2eca2
설혜린 <seolhyelin@seolhyelin-ui-MacBook-Pro-2.local>
2022-03-01 22:01:46 +0900

    Add: 랜덤이미지 멈추기 기능

--- practice1/src/index.js
+++ practice1/src/index.js
@@ -1,16 +1,30 @@
 function game() {
   const startBtn = document.querySelector(".start-btn");
   const shuffle = document.querySelector(".shuffle");
+  const btnList = document.querySelector(".button-list");
 
+  let timer = null;
   let slideImg = document.querySelector(".slideImg");
   let imgArr = [
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyNIQL-P7OBBMLsZW_V1F2iDgSBFQc6WcNTYa37m5EaE-y2ZK_1PFVV1d66GmceEWb8OA&usqp=CAU",
     "https://sg-res.9appsdownloading.com/sg/res/jpg/e0/ec/411e206783d152e6806594eee37b-8yc1.jpg?x-oss-process=style/mq200",
     "https://blog.kakaocdn.net/dn/HcCJo/btqXD4Lybq6/kcrH6skAoOD9oAq2QYfBx1/paper.png?attach=1&knm=img.png",
   ];
-  setInterval(() => {
-    const img = imgArr[Math.floor(Math.random() * 3)];
-    slideImg.src = img;
-  }, 200);
+
+  const start = () => {
+    timer = setInterval(() => {
+      const img = imgArr[Math.floor(Math.random() * 3)];
+      slideImg.src = img;
+    }, 200);
+  };
+
+  startBtn.addEventListener("click", (e) => {
+    start();
+  });
+
+  btnList.addEventListener("click", (e) => {
+    console.log("dd");
+    clearInterval(timer);
+  });
 }
 game();
