3f4c1509f3c2844769495dd078b2c3714da168df
설혜린 <tosiltosilkk@gmail.com>
2022-03-06 23:27:17 +0900

    Add: 버튼 클릭 시 이미지 멈춤

--- practice1/src/index.js
+++ practice1/src/index.js
@@ -5,25 +5,39 @@ function game() {
 
   let timer = null;
   let slideImg = document.querySelector(".slideImg");
+  let prevName = "scissors";
   let imgArr = [
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyNIQL-P7OBBMLsZW_V1F2iDgSBFQc6WcNTYa37m5EaE-y2ZK_1PFVV1d66GmceEWb8OA&usqp=CAU",
-    "https://sg-res.9appsdownloading.com/sg/res/jpg/e0/ec/411e206783d152e6806594eee37b-8yc1.jpg?x-oss-process=style/mq200",
-    "https://blog.kakaocdn.net/dn/HcCJo/btqXD4Lybq6/kcrH6skAoOD9oAq2QYfBx1/paper.png?attach=1&knm=img.png",
+    {
+      shape: "✌🏼",
+      name: "scissors",
+    },
+    {
+      shape: "✊🏼",
+      name: "rock",
+    },
+    {
+      shape: "✋🏼",
+      name: "paper",
+    },
   ];
 
   const start = () => {
     timer = setInterval(() => {
       const img = imgArr[Math.floor(Math.random() * 3)];
-      slideImg.src = img;
+      slideImg.innerHTML = img.shape;
+      slideImg.classList.replace(prevName, img.name);
+      prevName = img.name;
     }, 200);
   };
+
+  const compare = (me) => {};
 
   startBtn.addEventListener("click", (e) => {
     start();
   });
 
   btnList.addEventListener("click", (e) => {
-    console.log("dd");
+    compare(e.target.classList[0]);
     clearInterval(timer);
   });
 }
